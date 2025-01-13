d95cb719f8adec3be3795ea6165c9d15c2564ed4
b-hyoung <youqlrqod@gmail.com>
2025-01-13 21:47:57 +0900

    add dongin

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LoginPage from './LoginPage/LoginPage.tsx';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage path = "/"/>
+    </div>
+  );
+}
+
+export default App;
